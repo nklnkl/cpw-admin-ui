5c37f2a08930801527949cf12c6039652d5b5154
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-19 06:33:13 -0400

    Add components to app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { InvoicesComponent } from './invoices/invoices.component';
+import { AgentsComponent } from './agents/agents.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: InvoicesComponent
+	},
+	{
+		path: 'invoices',
+		component: InvoicesComponent
+	},
+	{
+		path: 'agents',
+		component: AgentsComponent
+	},
+	{
+		path: 'profile',
+		component: ProfileComponent
+	},
+	{
+		path: 'login',
+		component: LoginComponent
+	},
+	{
+		path: 'register',
+		component: RegisterComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
