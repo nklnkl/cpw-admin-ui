6ed839c3e7204e01af714ea3fa8e674dc91fdae3
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-18 23:07:39 -0400

    Create Agents component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoicesComponent } from './invoices/invoices.component';
+import { AgentsComponent } from './agents/agents.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    InvoicesComponent
+    InvoicesComponent,
+    AgentsComponent
   ],
   imports: [
     BrowserModule,
