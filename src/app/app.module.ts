7ca32842f710f68c69147409f6a98eef17d9514b
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-18 23:08:00 -0400

    Create Profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InvoicesComponent } from './invoices/invoices.component';
 import { AgentsComponent } from './agents/agents.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     InvoicesComponent,
-    AgentsComponent
+    AgentsComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
