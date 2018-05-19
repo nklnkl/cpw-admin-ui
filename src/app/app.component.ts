05d80461a9642d262b58bc7de6ff4e1522417c3a
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-18 22:56:36 -0400

    Add mobile menu dropdown functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+	mobileNav: boolean = false;
+
+	toggleNav () {
+		this.mobileNav = !this.mobileNav;
+	}
 }
