b0f89efea16019de2e8dd0346f4af518c8b9bed3
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-22 05:19:03 -0400

    Add invoices array to Invoices component

--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Invoice } from '../invoice';
 
 @Component({
   selector: 'app-invoices',
@@ -8,8 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class InvoicesComponent implements OnInit {
 
 	agentNumber: string;
+	invoices: Array<Invoice>;
 
-  constructor() { }
+	constructor() {
+		this.invoices = new Array<Invoice>();
+		let invoice: Invoice = new Invoice();
+		invoice.name = "lol";
+		invoice.phone = "23231231231";
+		invoice.total = 23232;
+		invoice.date = 3442432424;
+		invoice.agent = 69;
+		this.invoices.push(invoice);
+	}
 
   ngOnInit() {
   }
