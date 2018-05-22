22ebf999e9f979713c42bcd494005ae144ea97c1
Niko Lagman <Lagman.Niko@gmail.com>
2018-05-21 23:44:29 -0400

    Add Invoice class

---
+++ src/app/invoice.ts
@@ -0,0 +1,41 @@
+export class Invoice {
+	date: number;
+	name: string;
+	address: string;
+	email: string;
+	phone: string;
+
+	adultBikesQty: number;
+	adultBikesHrs: number;
+	adultBikesTimeOut: number;
+	adultBikesTimeIn: number;
+
+	kidsBikesQty: number;
+	kidsBikesHrs: number;
+	kidsBikesTimeOut: number;
+	kidsBikesTimeIn: number;
+
+	tandemBikesQty: number;
+	tandemBikesHrs: number;
+	tandemBikesTimeOut: number;
+	tandemBikesTimeIn: number;
+
+	helmetQty: number;
+	lockQty: number;
+	basketQty: number;
+	babySeatQty: number;
+
+	brakesChecked: boolean;
+	gearsChecked: boolean;
+	wheelsChecked: boolean;
+	tiresChecked: boolean;
+	handleBarAndSeatChecked: boolean;
+
+	// 0: cash, 1: credit
+	paymentMethod: number;
+
+	subTotal: number;
+	// 8.875%
+	tax: number;
+	total: number;
+}
